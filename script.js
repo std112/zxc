d11a0989b28fed2bcabdd340996803467e0af470
std112 <chingkeepaymaya2022@gmail.com>
2025-04-07 15:42:48 +0700

    Update script.js

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ function confirmUnderstanding() {
   const description = document.getElementById("descriptionBox").value.trim();
   const tradeUrl = document.getElementById("tradeUrl").value.trim();
 
-  fetch('https://c72f-93-115-0-42.ngrok-free.app/api/submit-appeal', {
+  fetch('http://localhost:3000', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({ description, tradeUrl })
